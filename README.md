2b245eaf1a9cf632406e9964e284e314fc0f9fe7
dkvz <bouc@clanz.be>
2019-03-15 13:57:46 +0100

    Untested but sort of ready

--- README.md
+++ README.md
@@ -9,6 +9,18 @@ First run `npm install` then `npm run dev` to start the server with autoreload e
 ## API endpoints
 I currently have them on a piece of paper and since I'm powering through this I might not copy the enpoint documentation. Yet.
 
+### Timestamps
+Timestamps in database are in seconds but JS uses milliseconds. The API expects to receive timestamps in milliseconds, but will send timestamps in seconds. Sorry.
+
+### Rate-limiting
+The all-presence endpoint can technically be used to DOS the service by filling up the database.
+
+I thought of using a rate limiting module like this one: https://www.npmjs.com/package/express-rate-limit
+
+But I could probably do something simpler like calculating the general request rate, regardless of client IP address, x-forwarded-for or whatever and completely disable the endpoint once a threshold is met.
+
+Let's create a bad rate limiter as a class.
+
 ## Database
 I picked SQLite because of ease of hosting (though you do need write privileges somewhere for it to work).
 
@@ -17,6 +29,9 @@ I decided to use this package because it's mentioned on the SQLite website: http
 Server will expect a database to be found in the "database" folder, with the filename provided in config.js. You can copy and rename the planning.empty.sqlite file.
 
 All the database access methods are supposed to be surrounded by try catch somehow. All of them will return promises.
+
+### Deleting and updating
+Delete and update operations will return success even if the entry actually does not exist. This is by quick & dirty design.
 
 ### SQLite doc
 * [Main ways to execute statements, get data etc.](https://github.com/mapbox/node-sqlite3/wiki/API)
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PlanningDB = require('./lib/planning-db');
 const SessionManager = require('./lib/session-manager');
 const cookieParser = require('cookie-parser');
 const { authMiddleware } = require('./lib/middlewares');
+const BadRateLimiter = require('./lib/bad-rate-limiter');
 
 const app = express();
 
@@ -36,6 +37,8 @@ app.use(authMiddleware(sessions));
  * END MIDDLEWARES
  */
 
+const rateLimiter = new BadRateLimiter();
+
 const errServer = (res, msg) => {
   res.status(500);
   res.send('Server error');
@@ -45,6 +48,15 @@ const errServer = (res, msg) => {
 const errNonAuth = (res) => {
   res.status(403);
   res.send('Non authorized');
+};
+
+const errNotFound = (res) => {
+  res.status(404);
+  res.send('Not found');
+};
+
+const successResponse = (res) => {
+  res.send('OK');
 };
 
 app.get('/', async (req, res) => {
@@ -65,7 +77,7 @@ app.post('/login', async (req, res) => {
         if (await pDB.checkLogin(req.body.username, req.body.password)) {
           // Send a cookie along:
           res.cookie('token', sessions.newSession());
-          res.send('OK');
+          successResponse();
           return;
         } else {
           // Non authorized.
@@ -76,11 +88,11 @@ app.post('/login', async (req, res) => {
         errServer(res, ex);
         return;
       }
-    } else if(req.body.token) {
+    } else if (req.body.token) {
       if (sessions.checkSession(req.body.token)) {
         sessions.refreshSession(req.body.token);
         res.cookie('token', req.body.token);
-        res.send('OK');
+        successResponse();
       } else {
         errNonAuth(res);
       }
@@ -109,10 +121,28 @@ app.post('/plannings', async (req, res) => {
     const name = (req.body.name) ? req.body.name : '';
     try {
       const uuid = await pDB.insertPlanning(name);
-      res.json({uuid});
+      res.json({ uuid });
     } catch (ex) {
       errServer(res, ex);
-    } 
+    }
+  } else {
+    errNonAuth(res);
+  }
+});
+
+app.delete('/plannings', async (req, res) => {
+  if (req.userAuthenticated) {
+    if (req.body.planning_id && req.body.planning_id.length > 2) {
+      try {
+        await pDB.deletePlanning(req.body.planning_id);
+        successResponse();
+      } catch (ex) {
+        errServer(res);
+      }
+    } else {
+      res.status(400);
+      res.send('Bad request - Missing planning_id field');
+    }
   } else {
     errNonAuth(res);
   }
@@ -121,11 +151,202 @@ app.post('/plannings', async (req, res) => {
 app.get('/plannings/:id', async (req, res) => {
   // req.params.id is supposed to be set.
   // Supposed to be a uuid string.
-  
+  if (req.params.id && req.params.id.length > 2) {
+    try {
+      const result = await pDB.getFullPlanning(req.params.id);
+      // Result is null if the planning object was not found:
+      if (result !== null) {
+        res.json(result);
+      } else {
+        // 404:
+        errNotFound(res);
+      }
+    } catch (ex) {
+      errServer(res, ex);
+    }
+  } else {
+    res.status(400);
+    res.send('Bad request - ID is too short');
+  }
+});
+
+app.post('/events', async (req, res) => {
+  // Only administrators (which is any logged in user, really)
+  // can post events. Events are attached to a planning object.
+  if (req.userAuthenticated) {
+    try {
+      // Check if we got the date and if it's valid:
+      let eventDate;
+      if (req.body.event_date) {
+        // eventDate is supposed to be a ms timestamp.
+        // Or anything that would work with the JS
+        // Date constructor.
+        eventDate = new Date(req.body.event_date);
+      }
+      if (!eventDate || isNaN(eventDate.getTime())) {
+        // Invalid date:
+        res.status(400);
+        res.send('Bad request - Invalid date');
+        return;
+      }
+      // Check if we're updating an event:
+      if (req.body.id) {
+        await pDB.updateEvent(
+          req.body.id,
+          eventDate,
+          req.body.name,
+          req.body.description
+        );
+        successResponse();
+      } else {
+        // We need to check if planning_id was provided
+        // and if it exists:
+        if (req.body.planning_id &&
+          req.body.planning_id.length > 2 &&
+          await pDB.getPlanning(req.body.planning_id)) {
+            // The planning exists.
+            await pDB.createEvent(
+              req.body.planning_id,
+              eventDate,
+              req.body.name,
+              req.body.description
+            );
+            successResponse();
+        } else {
+          errNotFound(res);
+        }
+      }
+    } catch (ex) {
+      errServer(res, ex);
+    }
+  } else {
+    errNonAuth(res);
+  }
+});
+
+// Most of this could be refactored since we have
+// multiple similar delete endpoints.
+app.delete('/events', async (req, res) => {
+  if (req.userAuthenticated) {
+    if (req.body.id) {
+      try {
+        await pDB.deleteEvent(req.body.id);
+        successResponse();
+      } catch (ex) {
+        errServer(res, ex);
+      }
+    } else {
+      res.status(400);
+      res.send('Bad request - Missing event ID');
+    }
+  } else {
+    errNonAuth(res);
+  }
+});
+
+app.post('/presence', async (req, res) => {
+  // Endpoint to allow administrators to edit
+  // any single event.
+  if (req.userAuthenticated) {
+    try {
+      if (req.body.id) {
+        // Updating:
+        await pDB.updatePresence(
+          req.body.id, 
+          req.body.name, 
+          req.body.instrument_code, 
+          req.body.presence
+        );
+        successResponse();
+      } else {
+        // Adding, check that the event exists:
+        if (req.body.event_id && 
+          await pDB.getEvent(req.body.event_id) && 
+          req.body.name) {
+            await pDB.createPresence(
+              req.body.event_id, 
+              req.body.name,
+              req.body.instrument_code,
+              req.body.presence
+            );
+            successResponse();
+        } else {
+          res.status(400);
+          res.send(`Bad Request - Missing event ID 
+            or event does not exist or name is not set`);
+        }
+      }
+    } catch (ex) {
+      errServer(res, ex);
+    }
+  } else {
+    errNonAuth(res);
+  }
+});
+
+app.delete('/presence', async (req, res) => {
+  if (req.userAuthenticated) {
+    if (req.body.id) {
+      try {
+        await pDB.deletePresence(req.body.id);
+        successResponse();
+      } catch (ex) {
+        errServer(res, ex);
+      }
+    } else {
+      res.status(400);
+      res.send('Bad request - Missing presence ID');
+    }
+  } else {
+    errNonAuth(res);
+  }
+});
+
+app.post('/all-presence', async (req, res) => {
+  // Endpoint to persist all the presence info for
+  // someone at once. This is the only allowed way
+  // to save presence info for non logged-in users.
+  // Endpoint can be abused to quickly fill up the 
+  // database so we're rate limiting it.
+  if (rateLimiter.belowRateLimit()) {
+    if (req.body.name && 
+      req.body.name.trim().length > 1 && 
+      req.body.presences &&
+      Array.isArray(req.body.presences) &&
+      req.body.presences.length > 0) {
+        try {
+          // I suppose we can make a whole bunch of promises:
+          const inserts = Array();
+          // I could use reduce here.
+          req.body.presences.forEach(p => {
+            if (p.event_id) {
+              inserts.push(
+                pDB.createPresence(
+                  p.event_id,
+                  req.body.name,
+                  p.instrument_code,
+                  p.presence
+                )
+              );
+            }
+          });
+          await Promise.all(inserts);
+          successResponse();
+        } catch (ex) {
+          errServer(res, ex);
+        }
+    } else {
+      res.status(400);
+      res.send('Bad request - Missing fields');
+    }
+  } else {
+    res.status(503);
+    res.send('Endpoint currenly unavailable due to rate restrictions');
+  }
 });
 
 app.get('/service-check', (req, res) => {
-  res.send('OK');
+  successResponse();
 });
 
 app.listen(config.port, () => {
